16b103e1c12bddbe6f7c4dace156ffc81522df1f
Kaychub <kaychubyt@gmail.com>
2021-06-17 22:45:27 -0700

    Multi prop passed

--- 02-components/src/index.js
+++ 02-components/src/index.js
@@ -6,9 +6,9 @@ import faker from 'faker';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
+      <CommentDetail author={faker.name.firstName()} timeAgo="4:45PM" avatar={faker.image.avatar()} commentText='Sad'/>
+      <CommentDetail author={faker.name.firstName()} timeAgo="5:00PM" avatar={faker.image.avatar()} commentText='Sad'/>
+      <CommentDetail author={faker.name.firstName()} timeAgo="6:00AM" avatar={faker.image.avatar()} commentText='Sad'/>
     </div>
   );
 };
