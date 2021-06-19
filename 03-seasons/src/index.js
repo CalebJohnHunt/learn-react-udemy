f4d58e2c8a39c84dcdc5699734e3a7df1f8c6749
Kaychub <kaychubyt@gmail.com>
2021-06-19 00:15:14 -0700

    Prettified including css

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
   // Same as using the constructor and this.state = { ... }
@@ -25,7 +26,7 @@ class App extends React.Component {
         return <div>Error: {this.state.errorMessage}</div>;
       } else
       if (this.state.lat && !this.state.errorMessage) {
-        return <div>Latitude: {this.state.lat}</div>;
+        return <SeasonDisplay lat={this.state.lat}/>;
       }
       return <div>Loading</div>;
   }
