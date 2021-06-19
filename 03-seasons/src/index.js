307954d9d7b19c29ae3d1e93b42761a22bec385a
Kaychub <kaychubyt@gmail.com>
2021-06-19 00:20:56 -0700

    Actually included css (sorry). Also implemented spinning loading icon

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   // Same as using the constructor and this.state = { ... }
@@ -28,7 +29,7 @@ class App extends React.Component {
       if (this.state.lat && !this.state.errorMessage) {
         return <SeasonDisplay lat={this.state.lat}/>;
       }
-      return <div>Loading</div>;
+      return <Spinner message="Please allow location access"/>;
   }
 }
 
