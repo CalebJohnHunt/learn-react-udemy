823061493a3b0d2091bf43ae3117feca01847f4a
Kaychub <kaychubyt@gmail.com>
2021-06-19 00:43:58 -0700

    Refactored from fuction to class

--- 03-seasons/src/SeasonDisplay.js
+++ 03-seasons/src/SeasonDisplay.js
@@ -1,36 +1,39 @@
 import './SeasonDisplay.css';
 import React from 'react';
 
-const seasonConfig = {
-  summer: {
-    text: 'Let\'s hit the beach!',
-    iconName: 'sun'
-  },
-  winter: {
-    text: 'Burr, it is chilly',
-    iconName: 'snowflake'
+
+class SeasonDisplay extends React.Component {
+  seasonConfig = {
+    summer: {
+      text: 'Let\'s hit the beach!',
+      iconName: 'sun'
+    },
+    winter: {
+      text: 'Burr, it is chilly',
+      iconName: 'snowflake'
+    }
+  };
+  
+  getSeason(lat, month) {
+    if (month > 2 && month < 9) {
+      return lat > 0 ? 'summer' : 'winter';
+    } else {
+      return lat > 0 ? 'winter' : 'summer';
+    }
   }
-};
 
-const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
-    return lat > 0 ? 'summer' : 'winter';
-  } else {
-    return lat > 0 ? 'winter' : 'summer';
+  render() {
+    const season = this.getSeason(this.props.lat, new Date().getMonth());
+    const { text, iconName } = this.seasonConfig[season];
+  
+    return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`} />
+    </div>
+    );
   }
-}
-
-const SeasonDisplay = (props) => {
-  const season = getSeason(props.lat, new Date().getMonth());
-  const { text, iconName } = seasonConfig[season];
-
-  return (
-  <div className={`season-display ${season}`}>
-    <i className={`${iconName} icon massive icon-left`} />
-    <h1>{text}</h1>
-    <i className={`${iconName} icon massive icon-right`} />
-  </div>
-  );
 };
 
 export default SeasonDisplay;
