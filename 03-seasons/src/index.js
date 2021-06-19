ae337ee82b173678aba329450d69bf45fa31c1f4
Kaychub <kaychubyt@gmail.com>
2021-06-18 23:31:54 -0700

    Added conditional renders. Finished section

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -6,22 +6,29 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
   
     window.navigator.geolocation.getCurrentPosition(
       (pos) => { // success callback
         // Don't do this.state.lat =
         this.setState({ lat: pos.coords.latitude });
       },
-      (err) => console.log(err)  // failure callback
+      (err) => { // failure callback
+        // Doesn't delete lat!
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // Must define render
   render() {
-    return (
-      <div>Latitude: {this.state.lat}</div>
-    );
+      if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: {this.state.errorMessage}</div>;
+      } else
+      if (this.state.lat && !this.state.errorMessage) {
+        return <div>Latitude: {this.state.lat}</div>;
+      }
+      return <div>Loading</div>;
   }
 }
 
